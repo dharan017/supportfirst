eecce015f9a59ff3abd7f08eb76ca30497b04de3
dharan017 <gold.dharani@gmail.com>
2017-08-20 18:49:31 +0530

    Menu bar and multi template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+//our root app component
+import { Component, NgModule, OnInit} from '@angular/core'
+import { RouterModule, Routes, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+ showComponent: boolean = true;
+ constructor(private router: Router) {}
+
+ ngOnInit() {
+  // listenging to routing navigation event
+  this.router.events.subscribe(event => this.modifyHeader(event));
+ }
+
+ modifyHeader(location) {
+   if (location.url=== "/login" || location.url === '/' )
+   {
+       this.showComponent = false;
+   } else {
+         this.showComponent = true;
+     }
+}  
+}
